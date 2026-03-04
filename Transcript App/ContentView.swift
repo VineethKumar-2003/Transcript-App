96bc14adefb129ed7c57d9449fd522d8bbe32568
Vineeth Kumar <135852071+VineethKumar-2003@users.noreply.github.com>
2026-03-04 09:53:27 +0530

    Initial Commit

---
+++ Transcript App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transcript App
+//
+//  Created by Vineeth Kumar G on 04/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transcript App/Transcript_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transcript_AppApp.swift
+//  Transcript App
+//
+//  Created by Vineeth Kumar G on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Transcript_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
